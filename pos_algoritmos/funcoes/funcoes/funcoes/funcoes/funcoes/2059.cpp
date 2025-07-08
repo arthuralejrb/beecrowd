aceeec76b2711e49f873b75e38f1e1360a9002f7
Árthur Balmaceda <arthuralejrb@gmail.com>
2025-07-08 16:55:08 -0300

    2059.cpp

--- pos_algoritmos/funcoes/funcoes/funcoes/funcoes/funcoes/2059.cpp
+++ pos_algoritmos/funcoes/funcoes/funcoes/funcoes/funcoes/2059.cpp
@@ -1,11 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+int cheat(int cheat, int accusation) {
+    if(cheat == 1 && accusation == 0 || cheat == 0 && accusation == 1) {
+        return 1;
+    }
+    return 2;
+}
+
 int main() {
 
+int p, j1, j2, c, a, sum = 0,result;
+cin>>p>>j1>>j2>>c>>a;
+sum = j1 + j2;
+
+if(p == 1 && sum % 2 == 0 || p == 0 && sum % 2 != 0){
+    result = 1;  
+}
+if(p == 1 && sum % 2 != 0 || p == 0 && sum % 2 == 0) {
+    result = 2;
+}
+
+if (c != 0 || a !=0) {
+    result = cheat(c, a);
+}
+
+cout<<"Jogador "<<result<<" ganha!"<<endl;
 
 
 
-    
 return 0;
 }
